6d146f98c3a50eb3bfb1a7caaaff50a4525cb283
manish-eqq <Manish@bol7.com>
2026-03-13 14:58:03 +0530

    Move migrate to deploy step, remove from build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -7,11 +7,3 @@ pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
-
-# Apply database migrations
-python manage.py migrate
-
-# Create superuser from environment variables (if set)
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
-    python manage.py createsuperuser --no-input || true
-fi
